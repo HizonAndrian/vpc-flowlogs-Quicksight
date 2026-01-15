b0a3e1050042e40de7944c8ba5814ca1c8ff91fa
[HizonAndrian] <[markandrianhizon@gmail.com]>
2026-01-15 10:24:46 +0800

    Added new queries.

--- athena_queries/date_stamp.sql
+++ athena_queries/date_stamp.sql
@@ -1,7 +1,17 @@
 SELECT
   date(from_unixtime(start)) AS log_date,
+  srcaddr,
   COUNT(*) AS rejected_requests
 FROM "vpc_flowlogs_db"."glue_catalog_table"
 WHERE action = 'REJECT'
-GROUP BY date(from_unixtime(start))
+GROUP BY date(from_unixtime(start)), srcaddr
+ORDER BY log_date;
+
+SELECT
+  date(from_unixtime(start)) AS log_date,
+  srcaddr,
+  COUNT(*) AS accepted_request
+FROM "vpc_flowlogs_db"."glue_catalog_table"
+WHERE action = 'ACCEPT'
+GROUP BY date(from_unixtime(start)), srcaddr
 ORDER BY log_date;
